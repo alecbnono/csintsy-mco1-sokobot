1f11b0d9ac1f4ba019a1ffc4980c15b2c0ce9681
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-09-18 10:29:23 +0800

    feat: added GameState.java and added comments for planning first

---
+++ src/solver/GameState.java
@@ -0,0 +1,11 @@
+package solver;
+
+public class GameState {
+    /*
+     * States Needed for the Game State:
+     * 1. Player Position using Point probably use a class for points
+     * 2. Box Position use a Hashset for uniqueness
+     * 3. Goal Position as well with Hashset uniqueness
+     * 4. Char move. ex: up = u, down = d, left = l, right = r
+     */
+}
