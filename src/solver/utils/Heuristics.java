2dc3dfc46b2c9c932223a2e1cf4688327a53dc89
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-10-11 02:52:37 +0800

    feat: add option for greedy assignment

--- src/solver/utils/Heuristics.java
+++ src/solver/utils/Heuristics.java
@@ -7,18 +7,12 @@ import solver.main.Point;
 public class Heuristics {
 
     private static int getAbsoulteValue(int number) {
-
-        if (number < 0) {
-            return number * -1;
-        } else
-            return number;
-
+        return number < 0 ? -number : number;
     }
 
     public static int getManhattan(Point position, Point target) {
         int differenceInX = getAbsoulteValue(position.getX() - target.getX());
         int differenceInY = getAbsoulteValue(position.getY() - target.getY());
-
         return differenceInX + differenceInY;
     }
 
@@ -26,27 +20,49 @@ public class Heuristics {
         int minDistance = Integer.MAX_VALUE;
         for (Point target : targets) {
             int distance = getManhattan(position, target);
-
             if (distance < minDistance) {
                 minDistance = distance;
             }
         }
-
         return minDistance;
     }
 
     public static int totalManhattan(HashSet<Point> boxes, HashSet<Point> targets) {
         int total = 0;
-
         for (Point box : boxes) {
             total += minimumManhattan(box, targets);
         }
-
         return total;
     }
 
     public static int getStateHeuristic(GameState state, HashSet<Point> targets) {
-        return totalManhattan(state.getBoxPosition(), targets);
-    }
+        // Greedy assignment heuristic
+        HashSet<Point> boxes = state.getBoxPosition();
+        HashSet<Point> remainingTargets = new HashSet<>(targets);
+        int heuristic = 0;
 
+        for (Point box : boxes) {
+            Point closestTarget = null;
+            int minDistance = Integer.MAX_VALUE;
+
+            for (Point target : remainingTargets) {
+                int distance = getManhattan(box, target);
+                if (distance < minDistance) {
+                    minDistance = distance;
+                    closestTarget = target;
+                }
+            }
+
+            if (closestTarget != null) {
+                heuristic += minDistance;
+                remainingTargets.remove(closestTarget);
+            }
+
+        }
+
+        return heuristic;
+
+        // Total Manhattan Heuristics
+        // return totalManhattan(boxes, targets);
+    }
 }
