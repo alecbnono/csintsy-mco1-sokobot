8c07a1dcc39ef38ef1ade222eccf05cb2518587a
Hannah Lee <hannahkimlee26@gmail.com>
2025-10-13 18:33:14 +0800

    Update GameState.java

--- src/solver/main/GameState.java
+++ src/solver/main/GameState.java
@@ -45,10 +45,6 @@ public class GameState {
         return heuristicValue;
     }
 
-    public LevelData getLevel() {
-        return level;
-    }
-
     private int computeHashCode() {
         int hash = 31 * playerPosition.hashCode();
         for (Point p : boxPosition)
