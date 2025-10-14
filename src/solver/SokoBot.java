1b2baa99f5abbfbfcd695c56ad21e0d42fc84501
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-10-14 18:51:14 +0800

    feat: temporary fix the issues (can work a star is still here)

--- src/solver/SokoBot.java
+++ src/solver/SokoBot.java
@@ -1,20 +1,33 @@
+package solver;
+
 import java.util.List;
 import solver.main.GameState;
 import solver.main.LevelData;
+import solver.searchAlgorithms.AstarAlgorithm;
 import solver.searchAlgorithms.GreedyBestFirstSearch;
 
 public class SokoBot {
 
-    public String solveSokobanPuzzle(int width, int height, char[][] mapData, char[][] itemsData) {
-        LevelData level = new LevelData(width, height, mapData, itemsData);
-        List<GameState> solution = GreedyBestFirstSearch.GBFS(level.getOrigin());
+  public String solveSokobanPuzzle(int width, int height, char[][] mapData, char[][] itemsData) {
+    LevelData level = new LevelData(width, height, mapData, itemsData);
+    AstarAlgorithm astar = new AstarAlgorithm();
 
+    // List<GameState> solution = astar.AstarSearch(level.getOrigin());
+    List<GameState> solution = GreedyBestFirstSearch.GBFS(level.getOrigin());
 
-
-        if (solution == null || solution.isEmpty()) {
-            return "No solution found.";
-@@ -26,4 +28,4 @@ public String solveSokobanPuzzle(int width, int height, char[][] mapData, char[]
-
-        return moves.toString();
+    if (solution == null || solution.isEmpty()) {
+      return "No solution found.";
     }
+
+    StringBuilder moves = new StringBuilder(solution.size() - 1);
+
+    GameState prev = solution.get(0);
+    for (int i = 1; i < solution.size(); i++) {
+      GameState curr = solution.get(i);
+      moves.append(curr.getMoveMadeFrom(prev));
+      prev = curr;
+    }
+
+    return moves.toString();
+  }
 }
--- src/solver/main/GameState.java
+++ src/solver/main/GameState.java
@@ -7,129 +7,133 @@ import solver.utils.Prune;
 
 public class GameState {
 
-    public static final int MAX_MOVES = 4;
-    private final int cachedHashCode;
-    private final LevelData level;
-    private final Point playerPosition;
-    private final HashSet<Point> boxPosition;
-    private final int heuristicValue;
-    private final char move;
+  public static final int MAX_MOVES = 4;
+  private final int cachedHashCode;
+  private final LevelData level;
+  private final Point playerPosition;
+  private final HashSet<Point> boxPosition;
+  private final int heuristicValue;
+  private final char move;
 
-    public GameState(Point playerPosition, HashSet<Point> boxPosition, LevelData level, char move) {
-        this(playerPosition, boxPosition, level, move,
-                Heuristics.totalManhattan(boxPosition, level.getTargets()));
+  public GameState(Point playerPosition, HashSet<Point> boxPosition, LevelData level, char move) {
+    this(playerPosition, boxPosition, level, move,
+        Heuristics.totalManhattan(boxPosition, level.getTargets()));
+  }
+
+  public GameState(Point playerPosition, HashSet<Point> boxPosition, LevelData level) {
+    this(playerPosition, boxPosition, level, ' ');
+  }
+
+  public GameState(Point playerPosition, HashSet<Point> boxPosition, LevelData level, char move, int heuristic) {
+    this.playerPosition = playerPosition;
+    this.boxPosition = new HashSet<>(boxPosition);
+    this.level = level;
+    this.move = move;
+    this.heuristicValue = heuristic;
+    this.cachedHashCode = computeHashCode();
+  }
+
+  public Point getPlayerPosition() {
+    return playerPosition;
+  }
+
+  public HashSet<Point> getBoxPosition() {
+    return boxPosition;
+  }
+
+  public int getHeuristicValue() {
+    return heuristicValue;
+  }
+
+  private int computeHashCode() {
+    int hash = 31 * playerPosition.hashCode();
+    for (Point p : boxPosition)
+      hash = 31 * hash + p.hashCode();
+    return hash;
+  }
+
+  public boolean isValidAction(char dir) {
+    Point next = playerPosition.pointAtMove(dir);
+    if (level.getWalls().contains(next))
+      return false;
+
+    if (boxPosition.contains(next)) {
+      Point nextNext = next.pointAtMove(dir);
+
+      // avoid pushing into walls
+      if (level.getWalls().contains(nextNext) || boxPosition.contains(nextNext))
+        return false;
+
+      // static deadlock check
+      if (level.getDeadlocks().contains(nextNext))
+        return false;
+
+      // dynamic deadlock check
+      // Only check frozen states if a box is actually moved into a new cell
+      if (Prune.isFrozen(level, boxPosition, next))
+        return false;
     }
 
-    public GameState(Point playerPosition, HashSet<Point> boxPosition, LevelData level) {
-        this(playerPosition, boxPosition, level, ' ');
+    return true;
+  }
+
+  public GameState generateState(char dir) {
+    if (!isValidAction(dir))
+      throw new IllegalArgumentException("Invalid move: " + dir);
+
+    Point nextPlayer = playerPosition.pointAtMove(dir);
+    HashSet<Point> newBoxes = boxPosition;
+    int newHeuristic = heuristicValue;
+
+    if (boxPosition.contains(nextPlayer)) {
+      Point from = nextPlayer;
+      Point to = nextPlayer.pointAtMove(dir);
+      newBoxes = new HashSet<>(boxPosition);
+      newBoxes.remove(from);
+      newBoxes.add(to);
+
+      // Incremental heuristic update
+      int oldDist = Heuristics.minimumManhattan(from, level.getTargets());
+      int newDist = Heuristics.minimumManhattan(to, level.getTargets());
+      newHeuristic += newDist - oldDist;
     }
 
-    public GameState(Point playerPosition, HashSet<Point> boxPosition, LevelData level, char move, int heuristic) {
-        this.playerPosition = playerPosition;
-        this.boxPosition = new HashSet<>(boxPosition);
-        this.level = level;
-        this.move = move;
-        this.heuristicValue = heuristic;
-        this.cachedHashCode = computeHashCode();
-    }
+    return new GameState(nextPlayer, newBoxes, level, dir, newHeuristic);
+  }
 
-    public Point getPlayerPosition() {
-        return playerPosition;
-    }
+  public ArrayList<GameState> getNextStates() {
+    ArrayList<GameState> states = new ArrayList<>(MAX_MOVES);
+    for (char move : new char[] { 'u', 'd', 'l', 'r' })
+      if (isValidAction(move))
+        states.add(generateState(move));
+    return states;
+  }
 
-    public HashSet<Point> getBoxPosition() {
-        return boxPosition;
-    }
+  public char getMoveMadeFrom(GameState previous) {
+    return this.move;
+  }
 
-    public int getHeuristicValue() {
-        return heuristicValue;
-    }
+  public LevelData getLevel() {
+    return this.level;
+  }
 
-    private int computeHashCode() {
-        int hash = 31 * playerPosition.hashCode();
-        for (Point p : boxPosition)
-            hash = 31 * hash + p.hashCode();
-        return hash;
-    }
+  @Override
+  public int hashCode() {
+    return cachedHashCode;
+  }
 
-    public boolean isValidAction(char dir) {
-        Point next = playerPosition.pointAtMove(dir);
-        if (level.getWalls().contains(next))
-            return false;
+  @Override
+  public boolean equals(Object o) {
+    if (this == o)
+      return true;
+    if (!(o instanceof GameState))
+      return false;
+    GameState g = (GameState) o;
+    return playerPosition.equals(g.playerPosition) && boxPosition.equals(g.boxPosition);
+  }
 
-        if (boxPosition.contains(next)) {
-            Point nextNext = next.pointAtMove(dir);
-
-            // avoid pushing into walls
-            if (level.getWalls().contains(nextNext) || boxPosition.contains(nextNext))
-                return false;
-
-            // static deadlock check
-            if (level.getDeadlocks().contains(nextNext))
-                return false;
-
-            // dynamic deadlock check
-            // Only check frozen states if a box is actually moved into a new cell
-            if (Prune.isFrozen(level, boxPosition, next))
-                return false;
-        }
-
-        return true;
-    }
-
-    public GameState generateState(char dir) {
-        if (!isValidAction(dir))
-            throw new IllegalArgumentException("Invalid move: " + dir);
-
-        Point nextPlayer = playerPosition.pointAtMove(dir);
-        HashSet<Point> newBoxes = boxPosition;
-        int newHeuristic = heuristicValue;
-
-        if (boxPosition.contains(nextPlayer)) {
-            Point from = nextPlayer;
-            Point to = nextPlayer.pointAtMove(dir);
-            newBoxes = new HashSet<>(boxPosition);
-            newBoxes.remove(from);
-            newBoxes.add(to);
-
-            // Incremental heuristic update
-            int oldDist = Heuristics.minimumManhattan(from, level.getTargets());
-            int newDist = Heuristics.minimumManhattan(to, level.getTargets());
-            newHeuristic += newDist - oldDist;
-        }
-
-        return new GameState(nextPlayer, newBoxes, level, dir, newHeuristic);
-    }
-
-    public ArrayList<GameState> getNextStates() {
-        ArrayList<GameState> states = new ArrayList<>(MAX_MOVES);
-        for (char move : new char[] { 'u', 'd', 'l', 'r' })
-            if (isValidAction(move))
-                states.add(generateState(move));
-        return states;
-    }
-
-    public char getMoveMadeFrom(GameState previous) {
-        return this.move;
-    }
-
-    @Override
-    public int hashCode() {
-        return cachedHashCode;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (!(o instanceof GameState))
-            return false;
-        GameState g = (GameState) o;
-        return playerPosition.equals(g.playerPosition) && boxPosition.equals(g.boxPosition);
-    }
-
-    @Override
-    public String toString() {
-        return "Player=" + playerPosition + ", Boxes=" + boxPosition + ", Move=" + move;
-    }
+  @Override
+  public String toString() {
+    return "Player=" + playerPosition + ", Boxes=" + boxPosition + ", Move=" + move;
+  }
 }
